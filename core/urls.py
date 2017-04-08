c2ca2ed859d32d7bc8e17792653f44c1a02bd7a0
Eric Williamson <ericrw96@vt.edu>
2017-04-08 19:13:45 -0400

    Change 1 thing

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [url(r'^save_data/$', api.save_data, name="save_data"),
                url(r'^get_cpu_for_instance/(\S+)/$', api.get_cpu_for_instance, name="get_cpu_for_instance"),
                url(r'^get_instances_summary/$', api.get_instances_summary, name="get_instances_summary"),
                url(r'^dashboard/$', views.dashboard, name="dashboard"),
-               url(r'ec2/$', views.ec2, name="ec2"),
+               url(r'^ec2/$', views.ec2, name="ec2"),
                url(r'^get_instance_details/$', api.get_instance_details, name="get_instance_details"),
                url(r'^get_all_time_series/$', api.get_all_time_series, name="get_all_time_series")
                ]
