468d33c8e02ae674626dba407e1013e4e44b6f41
John Purviance <ffdd4846@vt.edu>
2017-04-08 02:30:06 -0400

    not safe now

--- core/api.py
+++ core/api.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseNotFound, HttpResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 
+@csrf_exempt
 def echo(request):
     post = request.Post
     print(post)
