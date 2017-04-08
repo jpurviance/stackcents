0696a770aba48acfd4b8ca32a8f3dc18c4ce523a
John Purviance <ffdd4846@vt.edu>
2017-04-08 15:58:33 -0400

    ec2 page is broke

--- core/views.py
+++ core/views.py
@@ -3,7 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def dashboard(request):
     return (render(request, "core/dashbard.html", {}))
+
+
+def ec2(request):
+    ec2_id = request.GET.get("id","-1")
+    context = {"id":ec2_id}
+    return render(request, "core/ec2.html",context)
