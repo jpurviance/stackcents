5355597ec1eedaa6fdcd07f6bc3e2bc7c22048b4
John Purviance <ffdd4846@vt.edu>
2017-04-08 02:31:34 -0400

    import

--- core/api.py
+++ core/api.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.http import HttpResponseNotFound, HttpResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def echo(request):
