034bcedde0620514523f0a4093a4d8393110a6b7
Eric Williamson <ericrw96@vt.edu>
2017-04-08 11:22:02 -0400

    Add dirty hack

--- core/api.py
+++ core/api.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseNotFound, HttpResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
-
+from stackcents.settings import countmap
 @csrf_exempt
 def echo(request):
     post = request.POST
@@ -12,9 +12,9 @@ def echo(request):
     return HttpResponse(status=200)
 
 
-countmap = collections.defaultdict(int)
+
 @csrf_exempt
 def count(request,number):
-    countmap[number] += 1
+    countmap[number] += int(number)
     return JsonResponse({"Count" : countmap[number]})
 
