cd8d7dea5209f88b2cc2c7a43f2c31d371e02155
Eric Williamson <ericrw96@vt.edu>
2017-04-09 01:11:09 -0400

    Remove some network crap that was fake anyways

--- core/static/core/js/dashboard.js
+++ core/static/core/js/dashboard.js
@@ -288,7 +288,7 @@ function populate_cloud_stats(json) {
     ec2s.sort(dynamicSort("id"));
     for (var i = 0; i < ec2s.length; i++) {
         var ec2 = ec2s[i];
-        var line = "<tr id=" + ec2["id"] + "><td>" + ec2["id"] + "</td><td>" + ec2["cpu"] + "</td><td>" + ec2["memory"] + "</td ><td>" + ec2["storage"] + "</td><td>" + ec2["network"] + "</td></tr>";
+        var line = "<tr id=" + ec2["id"] + "><td>" + ec2["id"] + "</td><td>" + ec2["cpu"] + "</td><td>" + ec2["memory"] + "</td ><td>" + ec2["storage"] + "</td></tr>";
         $("#ec2_total").append(line);
         $("#" + ec2["id"]).click(function (event) {
             window.location.href = "/ec2/?instance=" + $(this)[0]["id"];
--- core/static/core/js/ec2.js
+++ core/static/core/js/ec2.js
@@ -202,7 +202,7 @@ function populate_cloud_stats(json) {
     var ec2s = json["total"];
     for (var i = 0; i < ec2s.length; i++) {
         var ec2 = ec2s[i];
-        var line = "<tr><td>" + ec2["id"] + "</td><td>" + ec2["cpu"] + "</td><td>" + ec2["memory"] + "</td ><td>" + ec2["storage"] + "</td><td>" + ec2["network"] + "</td></tr>";
+        var line = "<tr><td>" + ec2["id"] + "</td><td>" + ec2["cpu"] + "</td><td>" + ec2["memory"] + "</td ><td>" + ec2["storage"] + "</td></tr>";
         $("#ec2_total").append(line);
     }
 }
