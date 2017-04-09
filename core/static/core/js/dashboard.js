629298fe035a07ff99424198724d6968331cf41d
Eric Williamson <ericrw96@vt.edu>
2017-04-09 01:04:12 -0400

    Breaking the dashboard

--- core/static/core/js/dashboard.js
+++ core/static/core/js/dashboard.js
@@ -273,7 +273,19 @@ var master_theme = {
 
 function populate_cloud_stats(json) {
     $("#ec2_total").empty();
+    function dynamicSort(property) {
+        var sortOrder = 1;
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a,b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
+    }
     var ec2s = json["total"];
+    ec2s.sort(dynamicSort("id"));
     for (var i = 0; i < ec2s.length; i++) {
         var ec2 = ec2s[i];
         var line = "<tr id=" + ec2["id"] + "><td>" + ec2["id"] + "</td><td>" + ec2["cpu"] + "</td><td>" + ec2["memory"] + "</td ><td>" + ec2["storage"] + "</td><td>" + ec2["network"] + "</td></tr>";
