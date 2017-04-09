8fbc1a1e1ce470c680591710d88f7a4e7fa26061
John Purviance <ffdd4846@vt.edu>
2017-04-09 01:26:50 -0400

    gone

--- core/static/core/js/dashboard.js
+++ core/static/core/js/dashboard.js
@@ -331,51 +331,6 @@ var chart;
 $(document).ready(function () {
 
     Highcharts.theme = master_theme;
-
-    Highcharts.chart('container', Highcharts.merge(Highcharts.theme, {
-
-        title: {
-            text: 'July temperatures'
-        },
-
-        xAxis: {
-            type: 'datetime'
-        },
-
-        yAxis: {
-            title: {
-                text: null
-            }
-        },
-
-        tooltip: {
-            crosshairs: true,
-            shared: true,
-            valueSuffix: '°C'
-        },
-
-        legend: {},
-
-        series: [{
-            name: 'Temperature',
-            data: averages,
-            zIndex: 1,
-            marker: {
-                fillColor: 'white',
-                lineWidth: 2,
-                lineColor: Highcharts.getOptions().colors[0]
-            }
-        }, {
-            name: 'Range',
-            data: ranges,
-            type: 'arearange',
-            lineWidth: 0,
-            linkedTo: ':previous',
-            color: Highcharts.getOptions().colors[0],
-            fillOpacity: 0.3,
-            zIndex: 0
-        }]
-    }));
     $.get("/get_instances_summary/", populate_cloud_stats);
     setInterval(function () {
         $.get("/get_instances_summary/", populate_cloud_stats);
